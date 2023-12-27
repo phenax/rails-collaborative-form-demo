feb2002208e6414cc9d246d801ee144711fff0fe
Akshay Nair <phenax5@gmail.com>
2023-12-27 12:25:12 +0530

    just build stuff to get started

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,4 +5,8 @@ class HomeController < ApplicationController
   def index
     p '---------------- page load ----------------'
   end
+
+  def healthz
+    render json: { status: 'OK' }, status: :ok
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root 'home#index'
+  get '/healthz', to: 'home#healthz'
 end
